aa7a614a2166062957be93e56579cc4dcd0f6321
Rakhita Wickramatunge <>
2022-05-16 14:35:11 +1200

    code tidy up work

--- app/feature-home/src/main/java/com/trademe/feature/home/internal/ui/main/MainViewModel.kt
+++ app/feature-home/src/main/java/com/trademe/feature/home/internal/ui/main/MainViewModel.kt
@@ -23,18 +23,22 @@ internal class MainViewModel : ViewModel(), LifecycleObserver {
     fun onBottomMenuClicked(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_discover ->{
+                item.isChecked = true
                 viewState.moveTo(LatestListing)
                 fragment.value = LATESTLISTING.fragmentClass
             }
             R.id.nav_notification ->{
+                item.isChecked = true
                 viewState.moveTo(Notification)
                 fragment.value = NOTIFICATIONS.fragmentClass
             }
             R.id.nav_watchlist ->{
+                item.isChecked = true
                 viewState.moveTo(Watchlist)
                 fragment.value = WATCHLIST.fragmentClass
             }
             R.id.nav_mytrademe ->{
+                item.isChecked = true
                 viewState.moveTo(MyTradeMe)
                 fragment.value = MYTRADEME.fragmentClass
             }
